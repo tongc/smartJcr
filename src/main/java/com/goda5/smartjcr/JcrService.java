719086efedd6e54e2329baeeb3b36adb5b2f21f5
chentongtc@gmail.com <chentongtc@gmail.com>
2017-01-07 21:22:21 +0000

    jcr service to set node properties automatically

--- src/main/java/com/goda5/smartjcr/JcrService.java
+++ src/main/java/com/goda5/smartjcr/JcrService.java
@@ -2,10 +2,25 @@ package com.goda5.smartjcr;
 
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
+import java.lang.reflect.Method;
+import java.util.Map;
 
 public class JcrService {
+    private final ReflectionService reflectionService;
+
+    public JcrService(ReflectionService reflectionService) {
+        this.reflectionService = reflectionService;
+    }
+
     public Node setProperties(Node node, Object object) throws RepositoryException {
-//        node.setProperty();
+        Map<Method, String> methodsWithFieldNamesAnnotationWith = reflectionService.getMethodsWithFieldNamesAnnotationWith(object, JcrProperty.class);
+        methodsWithFieldNamesAnnotationWith.forEach((method, name) -> {
+            try {
+                node.setProperty(name, String.valueOf(method.invoke(object)));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
         return node;
     }
 }
